f6c7abb253bf426bd3343b17a31e16cc465225d9
lisan97 <553841913@qq.com>
2022-01-09 22:03:08 +0800

    buildTree

--- binary_tree/889根据前序和后序遍历构造二叉树.py
+++ binary_tree/889根据前序和后序遍历构造二叉树.py
@@ -16,6 +16,8 @@ class Solution(object):
         #防止preorder只有一个值，取leftid 超出index的情况
         if len(preorder) == 1:
             return TreeNode(preorder[0])
+        #我们假设前序遍历的第二个元素是左子树的根节点，但实际上左子树可能是空指针，这个元素可能是右子树的根节点。
+        #由于这里无法确切进行判断，所以导致了最终答案的不唯一。
         root = TreeNode(preorder[0])
         #先序遍历的第二个节点是左子树的根节点，在后序遍历中位于左子树的最后一个，可以用来分割左右子树
         leftid = postorder.index(preorder[1])
