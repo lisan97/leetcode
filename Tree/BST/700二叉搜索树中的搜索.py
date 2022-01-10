5d240a02d6ed105639a119c72d4768a8f917a347
lisan97 <553841913@qq.com>
2022-01-10 23:32:46 +0800

    BST

---
+++ Tree/BST/700二叉搜索树中的搜索.py
@@ -0,0 +1,18 @@
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    def searchBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+        if not root:
+            return None
+        if root.val == val:
+            return root
+        return self.searchBST(root.left if root.val > val else root.right,val)
