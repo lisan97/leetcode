2bdc71604123b410dc9fb4608e9e84aa1b0422bf
lisan97 <553841913@qq.com>
2022-02-04 22:49:02 +0800

    twosum

---
+++ array/twosum/170两数之和3-数据结构设计.py
@@ -0,0 +1,31 @@
+class TwoSum(object):
+    def __init__(self):
+        from collections import defaultdict
+        self.freq = defaultdict(int)
+        
+    def add(self,number):
+        self.freq[number] += 1
+
+    def find(self,value):
+        for key in self.freq:
+            other = value - key
+            if other == key and self.freq[key] > 1:
+                return True
+            elif other != key and key in self.freq:
+                return True
+            else:
+                return False
+
+#查询频繁的场景
+class TwoSum(object):
+    def __init__(self):
+        self.sum = set()
+        self.nums = []
+
+    def add(self,number):
+        for n in self.nums:
+            self.sum.add(n + number)
+        self.nums.append(number)
+
+    def find(self,value):
+        return value in self.sum
---
+++ array/twosum/1两数相加.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dic = {}
+        #构造一个哈希表：元素映射到相应的索引
+        for i in range(len(nums)):
+            dic[nums[i]] = i
+        for i in range(len(nums)):
+            other = target - nums[i]
+            #如果 other 存在且不是 nums[i] 本身
+            if other in dic and dic[other] != i:
+                return [i,dic[other]]
