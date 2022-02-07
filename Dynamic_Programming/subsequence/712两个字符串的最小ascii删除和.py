e04c67c9c79534e6dd1c6ba69c11a8a62f431a30
lisan97 <553841913@qq.com>
2022-02-08 00:26:37 +0800

    subsequence

--- Dynamic_Programming/subsequence/712两个字符串的最小ascii删除和.py
+++ Dynamic_Programming/subsequence/712两个字符串的最小ascii删除和.py
@@ -8,6 +8,7 @@ class Solution(object):
         """
         m = len(s1)
         n = len(s2)
+        #base case dp[0][..], dp[..][0]
         dp = [[0] * (n+1) for _ in range(m+1)]
         for i in range(1,m+1):
             dp[i][0] = dp[i-1][0] + ord(s1[i-1])
@@ -16,8 +17,10 @@ class Solution(object):
         for i in range(1,m+1):
             for j in range(1,n+1):
                 if s1[i-1] == s2[j-1]:
+                    #s1[i-1] 和 s2[j-1] 都是在 lcs 中的，不用删除
                     dp[i][j] = dp[i-1][j-1]
                 else:
+                    #s1[i-1] 和 s2[j-1] 至少有一个不在 lcs 中，删一个和更小的
                     dp[i][j] = min(dp[i-1][j]+ord(s1[i-1]),dp[i][j-1]+ord(s2[j-1]))
         return dp[-1][-1]
 
