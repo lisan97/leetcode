ecb7018a68ec2b9c33b14ca102c70b25a3bb19bb
lisan97 <553841913@qq.com>
2022-02-15 00:04:44 +0800

    game

---
+++ Dynamic_Programming/312戳气球.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def maxCoins(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        #状态：气球区间(i,j)
+        #选择：戳哪个
+        #dp[i][j]表示(i,j)内的最大值
+        #base case，j<=i+1无球可戳，dp[i][j] =  0
+        #状态转移：关键在于dp数组的定义，需要避免子问题互相影响，所以我们反向思考,最后戳哪个球得分最高
+        n = len(nums)
+        #添加两侧的虚拟气球,因此(i,j)是开区间
+        points = [1]*(n+2)
+        for i in range(1,n+1):
+            points[i] = nums[i-1]
+        dp = [[0]*(n+2) for _ in range(n+2)]
+        #dp[i][j]所依赖的状态是dp[i][k]和dp[k][j]，因此从下往上遍历
+        for i in range(n,-1,-1):
+            for j in range(i+1,n+2):
+                #最后戳破的气球是哪个？
+                for k in range(i+1,j):
+                    dp[i][j] = max(dp[i][j],dp[i][k]+dp[k][j]+points[i]*points[k]*points[j])
+        return dp[0][n+1]
---
+++ Dynamic_Programming/877石子游戏.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def stoneGame(self, piles):
+        """
+        :type piles: List[int]
+        :rtype: bool
+        """
+        #状态有三个：开始的索引 i，结束的索引 j，当前轮到的人
+        #选择有两个：选择最左边的那堆石头，或者选择最右边的那堆石头
+        #dp[i][j][0]在(i,j)石堆间，先手能拿到的最高分
+        #dp[i][j][1]在(i,j)石堆间，后手能拿到的最高分
+        #dp[i][j][0] = max(piles[i]+dp[i+1][j][1],piles[j]+dp[i][j-1])
+        n = len(piles)
+        dp = [[[0]*2 for _ in range(n)] for _ in range(n)]
+        #base case：只有一堆石头
+        for i in range(n):
+            dp[i][i][0] = piles[i]
+        for i in range(n-2,-1,-1):
+            for j in range(i+1,n):
+                # 解释：我作为先手，面对 piles[i...j] 时，有两种选择：
+                # 要么我选择最左边的那一堆石头，然后面对 piles[i+1...j]
+                # 但是此时轮到对方，相当于我变成了后手；
+                # 要么我选择最右边的那一堆石头，然后面对 piles[i...j-1]
+                # 但是此时轮到对方，相当于我变成了后手。
+                # 解释：我作为后手，要等先手先选择，有两种情况：
+                # 如果先手选择了最左边那堆，给我剩下了 piles[i+1...j]
+                # 此时轮到我，我变成了先手；
+                # 如果先手选择了最右边那堆，给我剩下了 piles[i...j-1]
+                # 此时轮到我，我变成了先手。
+                left = piles[i] + dp[i+1][j][1]
+                right = piles[j] + dp[i][j-1][1]
+                if left > right:
+                    dp[i][j][0] = left
+                    dp[i][j][1] = dp[i+1][j][0]
+                else:
+                    dp[i][j][0] = right
+                    dp[i][j][1] = dp[i][j-1][0]
+        return dp[0][n-1][0] - dp[0][n-1][1] > 0
