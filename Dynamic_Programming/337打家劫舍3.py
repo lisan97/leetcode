9fa4832c2a1fc563d0939bf109356937883bb7ef
lisan97 <553841913@qq.com>
2022-02-15 23:42:56 +0800

    rob

--- Dynamic_Programming/337打家劫舍3.py
+++ Dynamic_Programming/337打家劫舍3.py
@@ -1,9 +1,9 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution:
     def rob(self, root: TreeNode) -> int:
         # 状态：在哪个节点
