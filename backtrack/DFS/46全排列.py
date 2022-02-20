a3fcd1d6b53d6c1a112a5bf5f492da7812b734db
lisan97 <553841913@qq.com>
2022-02-20 23:59:23 +0800

    dfs

---
+++ backtrack/DFS/46全排列.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        #记录结果
+        self.res = []
+        #记录路径
+        track = []
+        self.backward(nums,track)
+        return self.res
+
+    def backward(self,nums,track):
+        #结束条件：nums 中的元素全都在 track 中出现
+        if len(track) == len(nums):
+            # 向 res 中添加 path 时需要拷贝一个新的列表，不能直接append(path)否则最终 res 中的列表都是空的。
+            self.res.append(track[:])
+            return
+        #选择列表：nums 中不存在于 track 的那些元素
+        for i in range(len(nums)):
+            if nums[i] in track:
+                continue
+            #做选择
+            track.append(nums[i])
+            #进入下一层决策树
+            self.backward(nums,track)
+            #取消选择
+            track.pop()
