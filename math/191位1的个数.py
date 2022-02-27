ae1c4b9b2d1406d8a7ac4d4e87af5a39ea4ec076
lisan97 <553841913@qq.com>
2022-02-28 00:40:39 +0800

    math

--- math/191位1的个数.py
+++ math/191位1的个数.py
@@ -4,6 +4,7 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        #一个循环不停地消除 1 同时计数，直到 n 变成 0 为止
         res = 0
         while n != 0:
             n = n & (n-1)
