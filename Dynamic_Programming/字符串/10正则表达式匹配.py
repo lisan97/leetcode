35f17577dc5b9fa5a1dad133af15d98636e46a48
lisan97 <553841913@qq.com>
2022-04-02 00:59:42 +0800

    dp

--- Dynamic_Programming/字符串/10正则表达式匹配.py
+++ Dynamic_Programming/字符串/10正则表达式匹配.py
@@ -49,3 +49,37 @@ class Solution(object):
                 return False
         self.memo[key] = res
         return self.memo[key]
+
+#迭代
+class Solution(object):
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        #dp[i][j]示以 i 截止的字符串是否可以被以 j 截止的正则表达式匹配
+        #base case:dp[0][0] = True,还没开始时当然是能匹配的
+        m = len(s)
+        n = len(p)
+        dp = [[False]*(n+1) for _ in range(m+1)]
+        dp[0][0] = True
+        for i in range(1,n+1):
+            #当i还没开始时，遇到*则只能匹配0次
+            if p[i-1] == '*':
+                dp[0][i] = dp[0][i-2]
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                #遇到p[j]为'.'的情况
+                if p[j-1] == '.':
+                    dp[i][j] = dp[i-1][j-1]
+                #遇到p[j]为字符的情况，则看这两个字符是否相等
+                elif p[j-1] != '*' :
+                    dp[i][j] = dp[i-1][j-1] and p[j-1] == s[i-1]
+                #p[j]为*，但p[j-1] != s[i]的情况，则只能选择匹配0次
+                elif p[j-2] != s[i-1] and p[j-2] != '.':
+                    dp[i][j] = dp[i][j-2]
+                #p[j]为*，且p[j-1] == s[i]的情况，则匹配1次(dp[i][j-1]),匹配多次(dp[i-1][j]),匹配0次(dp[i][j-2])都可以
+                else:
+                    dp[i][j] = dp[i][j-1] or dp[i-1][j] or dp[i][j-2]
+        return dp[-1][-1]
